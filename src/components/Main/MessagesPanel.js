13fba776d2f3de45f8480507f9e91d698c0cfee1
Paulo Barata <pbara7a@gmail.com>
2022-07-29 11:16:41 +0100

    Chat panel scrolls to the last message received or sent.

--- src/components/Main/MessagesPanel.js
+++ src/components/Main/MessagesPanel.js
@@ -2,37 +2,47 @@ import { useContacts } from "../contexts/ContactsContext";
 import { useConversations } from "../contexts/ConversationsContext";
 import { useUserLoggedIn } from "../contexts/UserLoggedInContext";
 import getMessageSender from "../../utils/getMessageSender";
+import { useCallback } from "react";
 
 const MessagesPanel = () => {
   const { messages } = useConversations();
   const { id } = useUserLoggedIn();
   const { contacts } = useContacts();
+  const setLastMessageRef = useCallback((messageDiv) => {
+    if (messageDiv) {
+      messageDiv.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
 
   return (
     <div className="msg-panel d-flex flex-column align-items-start">
-      {messages.map((message) => (
-        <div
-          key={message.id}
-          className={`my-1 d-flex flex-column ${
-            message.senderId === id ? "align-self-end" : ""
-          }`}
-        >
+      {messages.map((message, i) => {
+        const lastMessage = messages.length - 1 === i;
+        return (
           <div
-            className={`rounded px-2 py-1 ${
-              message.senderId === id ? "message-me" : "message-others"
+            key={message.id}
+            ref={lastMessage ? setLastMessageRef : null}
+            className={`my-1 d-flex flex-column ${
+              message.senderId === id ? "align-self-end" : ""
             }`}
           >
-            {message.text}
+            <div
+              className={`rounded px-2 py-1 ${
+                message.senderId === id ? "message-me" : "message-others"
+              }`}
+            >
+              {message.text}
+            </div>
+            <div
+              className={`text-muted small ${
+                message.senderId === id ? "text-end" : "text-start"
+              }`}
+            >
+              {getMessageSender(message, id, contacts)}
+            </div>
           </div>
-          <div
-            className={`text-muted small ${
-              message.senderId === id ? "text-end" : "text-start"
-            }`}
-          >
-            {getMessageSender(message, id, contacts)}
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
